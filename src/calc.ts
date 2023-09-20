c2d0e27227e5f973353638f8ab3e24f2eaf8a581
Paulojft94 <1222013@isep.ipp.pt>
2023-09-20 19:45:02 +0100

    projecto2

--- src/calc.ts
+++ src/calc.ts
@@ -1,7 +1,7 @@
 export function sumOfArray(num: number[]): number {
-  let arrSum: number = 0;
+  let arraySum: number = 0;
   for (let i = 0; i < num.length; i++) {
-    arrSum += num[i];
+    arraySum += num[i];
   }
-  return arrSum;
+  return arraySum;
 }
