9bb072d7295508f7c0f2f0c6e920277d5a8a2eaf
Rita Castro <1222643@isep.ipp.pt>
2023-09-20 20:19:54 +0100

    changed my mind

--- src/calc.ts
+++ src/calc.ts
@@ -1,7 +1,7 @@
-export function sumOfArray(num: number[]): any {
+export function sumOfArray(num: number[]): number {
   let arraySum: number = 0;
   for (let i = 0; i < num.length; i++) {
     arraySum = arraySum +num[i];
   }
-  return 'cowboy';
+  return arraySum;
 }
