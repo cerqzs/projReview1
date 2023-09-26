5511e930714cf7bf98df0e849770c6ee4df187ee
Nuno Madaleno <1222639@isep.ipp.pt>
2023-09-26 23:25:05 +0100

    Files: prettierignore; prettierrc; setting.json;

--- src/calc.ts
+++ src/calc.ts
@@ -1,7 +1,7 @@
 export function sumOfArray(num: number[]): number {
-  let arraySum: number = 0;
-  for (let i = 0; i < num.length; i++) {
-    arraySum = arraySum +num[i];
-  }
-  return arraySum;
+    let arraySum: number = 0;
+    for (let i = 0; i < num.length; i++) {
+        arraySum = arraySum + num[i];
+    }
+    return arraySum;
 }
