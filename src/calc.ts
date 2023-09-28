512ac9ab8815d8fbe234f67f57e53e5c9ef6d23a
Nuno Madaleno <1222639@isep.ipp.pt>
2023-09-28 21:28:22 +0100

    test new branch; return change;

--- src/calc.ts
+++ src/calc.ts
@@ -1,7 +1,7 @@
-export function sumOfArray(num: number[]): number {
+export function sumOfArray(num: number[]): number | string {
     let arraySum: number = 0;
     for (let i = 0; i < num.length; i++) {
         arraySum = arraySum + num[i];
     }
-    return arraySum;
+    return "O somatório do array é: " + arraySum;
 }
